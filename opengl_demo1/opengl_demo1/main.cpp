9745c28b68036940669c489c5d17c82922b1af51
yeyongping <burnedfrog@gmail.com>
2021-07-23 00:45:06 +0800

    init

---
+++ opengl_demo1/opengl_demo1/main.cpp
@@ -0,0 +1,156 @@
+//
+//  main.cpp
+//  opengl_demo1
+//
+//  Created by Ye Yongping on 2021/7/22.
+//
+
+#define GLEW_STATIC
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+
+#define numVAOs 1
+
+using namespace std;
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+
+void printShaderLog(GLuint shader) {
+    int len = 0;
+    int chWriten = 0;
+    char *log;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetShaderInfoLog(shader, len, &chWriten, log);
+        cout << "Shader Info Log: " << log << endl;
+        free(log);
+    }
+}
+
+void printProgramLog(int prog) {
+    int len = 0;
+    int chWrittn = 0;
+    char* log;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetProgramInfoLog(prog, len, &chWrittn, log);
+        cout << "Program Info Log: " << log << endl;
+        free(log);
+    }
+}
+
+bool checkOpenGLError() {
+    bool foundError = false;
+    int glErr = glGetError();
+    while (glErr != GL_NO_ERROR) {
+        cout << "glError: " << glErr  << endl;
+        foundError = true;
+        glErr = glGetError();
+    }
+    return foundError;
+}
+
+string readShaderSource(const char* filePath) {
+    string content;
+    ifstream fileStream(filePath, ios::in);
+    string line = "";
+    while (!fileStream.eof()) {
+        getline(fileStream, line);
+        content.append(line + "\n");
+    }
+    fileStream.close();
+    return content;
+}
+
+GLuint createShaderProgram() {
+    GLint vertCompiled;
+    GLint fragCompiled;
+    GLint linked;
+    
+    string verShaderStr = readShaderSource("vertShader.glsl");
+    string fragShaderStr = readShaderSource("fragShader.glsl");
+    const char* vshaderSource = verShaderStr.c_str();
+    const char* fshaderSouce = fragShaderStr.c_str();
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSouce, NULL);
+    glCompileShader(vShader);
+    checkOpenGLError();
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+    if (vertCompiled != 1) {
+        cout << "vertex compilation failed" << endl;
+        printShaderLog(vShader);
+    }
+    glCompileShader(fShader);
+    checkOpenGLError();
+    glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+    if (fragCompiled != 1) {
+        cout << "fragment compilation failed" << endl;
+        printShaderLog(fShader);
+    }
+    
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+    checkOpenGLError();
+    glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+    if (linked != 1) {
+        cout << "linking failed" << endl;
+        printProgramLog(vfProgram);
+    }
+    return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+//    glClearColor(1.0, 0.0, 0.0, 1.0);
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glPointSize(100.0f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+int main(int argc, const char * argv[]) {
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OPENGL DEMO1", NULL, NULL);
+    int actualScreenWidth, actualScreenHeight;
+    glfwGetFramebufferSize(window, &actualScreenWidth, &actualScreenHeight);
+    glfwMakeContextCurrent(window);
+    glViewport(0, 0, actualScreenWidth, actualScreenHeight);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+    glfwSwapInterval(1);
+    
+    init(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
